eee58eb6a8e4570f541a4ae1f82e9b380ea55837
imilicevic <ivanmilicevicmeda@gmail.com>
2024-04-09 10:41:31 +0200

    Marketing Skeleton

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="hidden lg:block h-20 w-full border-t-2 bg-slate-200 p-2">
+      Footer
+    </div>
+  );
+};
---
+++ app/(marketing)/_components/header.tsx
@@ -0,0 +1,5 @@
+export const Header = () => {
+  return (
+    <header className="h-20 w-full border-b-2 bg-slate-200 px-4">Header</header>
+  );
+};

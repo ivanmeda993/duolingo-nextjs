d9613a1b736c7995a2449c6ca32c6cd66c34081c
imilicevic <ivanmilicevicmeda@gmail.com>
2024-04-24 12:52:25 +0200

    Schema

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -15,17 +15,14 @@ const LearnPage = async () => {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{
-            title: "Spanish",
-            imageSrc: "/rs.svg",
-          }}
+          activeCourse={userProgress.activeCourse}
           hasActiveSubscription={false}
-          hearts={5}
-          points={100}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="Spanish" />
+        <Header title={userProgress.activeCourse.title} />
       </FeedWrapper>
     </div>
   );
